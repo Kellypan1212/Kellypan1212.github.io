389ceebab7ae95f027c8a23cd48ca34057b478d1
Kellypan1212 <Kellypan121212@gmail.com>
2026-06-22 23:12:51 +0800

    uglybootstrapbyebye

--- js/site.js
+++ js/site.js
@@ -1,11 +1,3 @@
-document.head.insertAdjacentHTML(
-    "beforeend",
-    `
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-  `
-);
-
 (function () {
     const currentPage = location.pathname.split("/").pop() || "index.html";
 
